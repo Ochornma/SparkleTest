fe0582ce663597fc5cb8aab42c899e202fa8aa0b
Promise Ochornma <ochornmapromise@gmail.com>
2024-07-09 16:59:22 +0100

    Update SparkleTests.swift

--- SparkleTests/SparkleTests.swift
+++ SparkleTests/SparkleTests.swift
@@ -19,19 +19,23 @@ final class SparkleTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_result_view_model(){
+            
+        let info = CharacterResponseInfo(count: 826, pages: 42, next: "https://rickandmortyapi.com/api/character/?page=2", prev: "https://rickandmortyapi.com/api/character/?page=2")
+        
+        let result = CharacterResponseResult(id: 1, name: "Rick Sanchez", status: .alive, species: .human, type: "", gender: .male, origin: .init(name: "Earth", url: "https://rickandmortyapi.com/api/location/1"), location: .init(name: "Earth", url: "https://rickandmortyapi.com/api/location/20"), image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg", episode: ["https://rickandmortyapi.com/api/episode/1"], url: "https://ricandmortyapi.com/api/character/1", created: "2017-11-04T18:48:46.250Z")
+        
+        let expected = CharacterResponse(info: info, results: [result])
+        let data = expected.results ?? []
+        let model = data.map({
+            return FilmCharacter(data: $0)
+        })
+        
+            
+        XCTAssertEqual(data[0].name, model[0].name)
+        XCTAssertEqual(data[0].origin?.name, model[0].origin)
+        XCTAssertEqual(data[0].location?.name, model[0].location)
         }
-    }
+
 
 }
